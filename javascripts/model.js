15dab1f9d97ac04934526acbe61694c8289814aa
Oliver Foster <oliverfoster@localhost.localdomain>
2014-10-02 22:35:52 +0100

    added model.js

--- javascripts/model.js
+++ javascripts/model.js
@@ -6,14 +6,36 @@
 
 		var obj = args[0];
 
-		var rtn = new mod( obj, args[1] || "global" );
-		for (var key in obj) {
-			addProperty (obj, rtn, key);
-		}
+		var rtn;
+
+		if (isArray(obj)) rtn = new modArray( obj, args[1] || "global" );
+		else rtn = new modObject( obj, args[1] || "global" );
 
 		return rtn;
 
 	};
+
+	function isArray(obj) {
+		if (typeof obj !== "object") return false;
+		if (obj instanceof Array) return true;
+		for (var key in obj) {
+			if (isNumeric(key)) return true;
+		}
+		return false;
+	}
+
+	function TypeOf(obj) {
+		var type = typeof obj;
+		if (type !== "object") return type;
+		for (var key in obj) {
+			if (isNumeric(key)) return 'array';
+		}
+		return 'object';
+	}
+
+	function isNumeric(obj) {
+		return !isNaN(parseFloat(obj)) && isFinite(obj);
+	}
 
 	function mod (from, name) {
 		Object.defineProperties(this, {
@@ -41,42 +63,72 @@
 				value: undefined,
 				writable: true,
 				enumerable: false
+			},
+			json: {
+				get: function() {
+					return this.__from;
+				},
+				set: function(v) {
+					this.__from = v;
+					this.sync('pull');
+				}
 			}
 		});
-		this.on("change", function(proxy, name, value, path) {
-			if (this.__parent) {
-				this.__parent.trigger("change", proxy, name, value, this.__parent.__name + "." + ((path ? path + "." : path) || "")  + name);
-			}
-		});
-		this.on("add", function(proxy, name, value, path) {
-			if (this.__parent) {
-				this.__parent.trigger("add", proxy, name, value, this.__parent.__name + "." + ((path ? path + "." : path) || "")  + name)
-			}
-		})
+		for (var key in from) {
+			addProperty (from, this, key);
+		}
 	};
 	_.extend(mod.prototype, Backbone.Events);
 	_.extend(mod.prototype, {
-		update: function() {
+		sync: function(type) {
 			var modelKeys = Object.keys(this);
 			var definedKeys = Object.keys(this.__defined);
-			//model to from
-			var diff = _.difference(modelKeys, definedKeys);
-			for (var i = 0; i < diff.length; i++) {
-				if (this[diff[i]] instanceof mod ) {
-					this.__from[diff[i]] = this[diff[i]].__from;
-				} else {
-					this.__from[diff[i]] = this[diff[i]];
+			switch(type) {
+			case undefined: case "both":
+				var diff = _.difference(modelKeys, definedKeys);
+				for (var i = 0; i < diff.length; i++) {
+					if (this[diff[i]] instanceof mod || this[diff[i]] instanceof modObject || this[diff[i]] instanceof modArray ) {
+						this.__from[diff[i]] = this[diff[i]].__from;
+					} else {
+						this.__from[diff[i]] = this[diff[i]];
+					}
+					delete this[diff[i]];
+					addProperty(this.__from, this, diff[i]);
+					this.trigger("add", diff[i], this.__from[diff[i]], this, this.__from, diff[i]);
+					if (typeof this.__from[diff[i]] == "object") this[diff[i]].sync("both");
 				}
-				delete this[diff[i]];
-				addProperty(this.__from, this, diff[i]);
-				this.trigger("add", this, diff[i], this.__from[diff[i]]);
-				if (typeof this.__from[diff[i]] == "object") this[diff[i]].update();
-			}
-			//from to model
-			var diff = _.difference(definedKeys, modelKeys);
-			for (var i = 0; i < diff.length; i++) {
-				addProperty(this.__from, this, diff[i]);
-				if (typeof this.__from[diff[i]] == "object") this[diff[i]].update();
+				//from to model
+				var diff = _.difference(definedKeys, modelKeys);
+				for (var i = 0; i < diff.length; i++) {
+					addProperty(this.__from, this, diff[i]);
+					if (typeof this.__from[diff[i]] == "object") this[diff[i]].sync("both");
+				}
+				break;
+			case "push":
+				//model to from
+				var diff = _.difference(modelKeys, definedKeys);
+				for (var i = 0; i < diff.length; i++) {
+					if (this[diff[i]] instanceof mod || this[diff[i]] instanceof modObject || this[diff[i]] instanceof modArray ) {
+						this.__from[diff[i]] = this[diff[i]].__from;
+					} else {
+						this.__from[diff[i]] = this[diff[i]];
+					}
+					delete this[diff[i]];
+					addProperty(this.__from, this, diff[i]);
+					this.trigger("add", diff[i], this.__from[diff[i]], this, this.__from, diff[i]);
+					if (typeof this.__from[diff[i]] == "object") this[diff[i]].sync("push");
+				}
+				break;
+			case "pull":
+				//from to model
+				var diff = _.difference(definedKeys, modelKeys);
+				for (var i = 0; i < diff.length; i++) {
+					addProperty(this.__from, this, diff[i]);
+					if (typeof this.__from[diff[i]] == "object") this[diff[i]].sync("pull");
+				}
+				break;
+			default:
+				throw new Error("Unsupported sync type: '" + type + "'");
 			}
 		},
 		set: function(path, value) {
@@ -87,16 +139,20 @@
 		        if (n in o) {
 		            o = o[n];
 		        } else {
-		        	var nobj = {};
-		            o[n] = new mod(nobj, n);
+		        	if ( isNumeric(a[i+1]) ) {
+			            o[n] = new modArray({}, n);
+			        } else {
+			        	var nobj = {};
+			            o[n] = new modObject([], n);
+			        }
 		            o[n].__parent = o;
-		            o.update();
+		            o.sync('push');
 		            o = o[n];
 		        }
 		    }
-		    if (value instanceof mod) value = mod.__from;
+		    if (value instanceof mod || value instanceof modObject || value instanceof modArray) value = mod.__from;
 		    o[a[a.length - 1]] = value;
-		    o.update();
+		    o.sync('push');
 		    return this;
 		},
 		get: function(path) {
@@ -117,6 +173,99 @@
 	})
 
 
+	function modObject(from, name) {
+		mod.call(this, from, name);
+		this.on("change", function(path, value, proxy, json, name) {
+			if (this.__parent) {
+				var cpath;
+				var bpath;
+				cpath = this.__name + "." + (path || name);
+				if (path === name) bpath = this.__name;
+				
+				this.__parent.trigger("ChangeProperty:"+cpath, cpath, value, proxy, proxy.__from, name);
+				this.__parent.trigger("ChangeObject:"+bpath, bpath, value, proxy, proxy.__from, name);
+				this.__parent.trigger("ChangeProperty", cpath, value, proxy, proxy.__from, name);
+				this.__parent.trigger("ChangeObject", bpath, value, proxy, proxy.__from, name);				
+			}
+		});
+		this.on("add", function(path, value, proxy, json, name) {
+			if (this.__parent) {
+				var cpath;
+				var bpath;
+				cpath = this.__name + "." + (path || name);
+				if (path === name) bpath = this.__name;
+
+				this.__parent.trigger("AddProperty:"+cpath, cpath, value, proxy, proxy.__from, name);
+				this.__parent.trigger("AddObject:"+bpath, bpath, value, proxy, proxy.__from, name);
+				this.__parent.trigger("AddProperty", cpath, value, proxy, proxy.__from, name);				
+				this.__parent.trigger("AddObject", bpath, value, proxy, proxy.__from, name);				
+			}
+		});
+	}
+	modObject.prototype = {};
+	_.extend(modObject.prototype, mod.prototype);
+	
+	function modArray(from, name) {
+		mod.call(this, from, name);
+		Object.defineProperties(this, {
+			'length': {
+				get: function() {
+					return this.__from.length;
+				},
+				set: function(v) {
+					this.__from.length = v;
+				}
+			}
+		});
+		this.on("change", function(path, value, proxy, json, name) {
+			if (this.__parent) {
+				var cpath;
+				var bpath;
+				if (isNumeric(path)) {
+					cpath = this.__name + "[" + name + "]";
+					bpath = this.__name;
+				} else {
+					cpath = this.__name + "." + path;
+					bpath = cpath;
+				}
+				this.__parent.trigger("ChangeProperty:"+cpath, cpath, value, proxy, proxy.__from, name);
+				this.__parent.trigger("ChangeObject:"+bpath, bpath, value, proxy, proxy.__from, name);
+				this.__parent.trigger("ChangeProperty", cpath, value, proxy, proxy.__from, name);
+				this.__parent.trigger("ChangeObject", bpath, value, proxy, proxy.__from, name);
+			}
+		});
+		this.on("add", function(path, value, proxy, json, name) {
+			if (this.__parent) {
+				var cpath;
+				var bpath;
+				if (isNumeric(path)) {
+					cpath = this.__name + "[" + name + "]";
+					bpath = this.__name;
+				} else {
+					cpath = this.__name + "." + path;
+					bpath = cpath;
+				}
+				this.__parent.trigger("AddProperty:"+cpath, cpath, value, proxy, proxy.__from, name);
+				this.__parent.trigger("AddObject:"+bpath, bpath, value, proxy, proxy.__from, name);
+				this.__parent.trigger("AddProperty", cpath, value, proxy, proxy.__from, name);
+				this.__parent.trigger("AddObject", bpath, value, proxy, proxy.__from, name);				
+			}
+		});
+	}
+	modArray.prototype = [];
+	_.extend(modArray.prototype, mod.prototype);
+	_.extend(modArray.prototype, {
+		push: function() {
+			this.constructor.prototype.push.apply(this, arguments);
+			this.sync("push");
+		},
+		splice: function() {
+			this.constructor.prototype.splice.apply(this, arguments);
+			this.sync("push");	
+		}
+	})
+
+
 	function addProperty(object, proxy, name) {
 		var childproxycache;
 		proxy.__defined[name] = true;
@@ -125,7 +274,8 @@
 				get: function() {
 					if (typeof object[name] == "object") {
 						if (childproxycache) return childproxycache;
-						childproxycache = new mod(object[name], name);
+						if (isArray(object[name])) childproxycache = new modArray(object[name], name);
+						else childproxycache = new modObject(object[name], name);
 						childproxycache.__parent = proxy;
 						return childproxycache;
 					} else {
@@ -133,9 +283,9 @@
 					}
 				},
 				set: function (value) {
-					proxy.trigger("change", proxy, name, value);
+					proxy.trigger("change", name, value, proxy, proxy.__from, name);
 					if (typeof value == "object") childproxycache = undefined;
-					if (value instanceof mod) value = mod.__from;
+					if (value instanceof mod || value instanceof modObject || value instanceof modArray) value = mod.__from;
 					object[name] = value;
 					return value;
 				},
